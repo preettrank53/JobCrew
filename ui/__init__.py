592a7adb1fa06d1ce127e49edb0e5b2e0c05a5d9
preetrank53 <preetrank53@gmail.com>
2026-04-18 15:08:05 +0530

    fix: remove orphaned inject_custom_css from ui package init

--- ui/__init__.py
+++ ui/__init__.py
@@ -2,7 +2,7 @@ from .layout import set_page_config, render_header, initialize_session_state, re
 from .sidebar import render_sidebar, render_resume_upload_section
 from .job_search import render_job_search_panel
 from .pipeline_runner import render_pipeline_runner
-from .styles import inject_custom_css, render_metric_bar, render_footer
+from .styles import render_metric_bar, render_footer
 from .tracker_dashboard import render_tracker_dashboard
 
 __all__ = [
@@ -14,7 +14,6 @@ __all__ = [
     "render_resume_upload_section",
     "render_job_search_panel",
     "render_pipeline_runner",
-    "inject_custom_css",
     "render_metric_bar",
     "render_footer",
     "render_tracker_dashboard"
